e9ab74039f9377fe4231412dbbdbc81d58d48d76
wick <wickerdj@gmail.com>
2015-08-17 21:28:14 -0500

    Completed basic tutorial

--- main.lua
+++ main.lua
@@ -1,25 +1,40 @@
 HC = require 'HardonCollider'
 
 function on_collide(dt, shape_a, shape_b)
-  -- determins which shape is the ball and which the paddle
-  local paddle
+  -- determine which shape is the ball and which the paddle
+  local other
 
   if shape_a == ball then
-    paddle = shape_b
+    other = shape_b
+  elseif shape_b == ball then
+    other = shape_a
   else
-    paddle = shape_a
+    return
   end
 
-  -- reflect the ball on the paddle
-  local px, py = paddle:center()
-  local bx, by = ball:center()
-  ball.velocity.x = -ball.velocity.x
-  ball.velocity.y = by - py
+  -- reset on goal
+  if other == goalLeft then
+    ball.velocity = { x = 100, y = 0 }
+    ball:moveTo(400, 300)
+  elseif other == goalRight then
+    ball.velocity = { x = -100, y = 0 }
+    ball:moveTo(400, 300)
+  elseif other == borderTop or other == borderBottom then
+    -- bounce off top and bottom
+    ball.velocity.y = -ball.velocity.y
+  else
+    -- bounce off paddle
+    local px,py = other:center()
+    local bx,by = ball:center()
+    local dy = by - py
+    ball.velocity.x = -ball.velocity.x
+    ball.velocity.y = dy
 
-  -- keep the ball at the same speed as before
-  local len = math.sqrt(ball.velocity.x^2 + ball.velocity.y^2)
-  ball.velocity.x = ball.velocity.x / len * 100
-  ball.velocity.y = ball.velocity.y / len * 100
+    -- keep the ball at the same speed as before
+    local len = math.sqrt(ball.velocity.x^2 + ball.velocity.y^2)
+    ball.velocity.x = ball.velocity.x / len * 100
+    ball.velocity.y = ball.velocity.y / len * 100
+  end
 end
 
 function love.load()
@@ -30,6 +45,11 @@ function love.load()
   paddleRight = Collider:addRectangle(770, 250, 20, 100)
 
   ball.velocity = {x = -100, y = 0}
+
+  borderTop = Collider:addRectangle(0, -100, 800, 100)
+  borderBottom = Collider:addRectangle(0, 600, 800, 100)
+  goalLeft = Collider:addRectangle(-100, 0, 100, 600)
+  goalRight = Collider:addRectangle(800, 0, 100, 600)
 end
 
 function love.update(dt)
